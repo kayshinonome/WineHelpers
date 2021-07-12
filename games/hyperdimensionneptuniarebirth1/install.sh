4c4e936fef0611baa88d87e4ff34fac207d0f881
tsukisuperior <tsukisuperior@tutanota.com>
2021-07-11 19:42:30 -0500

    fixed the audio

--- games/hyperdimensionneptuniarebirth1/install.sh
+++ games/hyperdimensionneptuniarebirth1/install.sh
@@ -3,4 +3,4 @@
 rm -rf "$WINEPREFIX"
 mkdir -pv ~/.winehelpers
 cd ~/.winehelpers || exit 1
-winetricks xact devenum quartz wmp9 vcrun2012 vcrun2013
+winetricks xact devenum quartz wmp9 vcrun2012 vcrun2013 windowscodecs
